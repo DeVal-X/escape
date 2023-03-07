0faf0d3baa2dc8b6a637c2b3c66e51a725989d1f
david rabuteau <david.rabuteau@hotmail.fr>
2023-03-07 17:49:49 +0100

    homepage pour rejoindre une game

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :games, only: %i[create new index show update ] do
+    collection do
+      get :join, to: "games#join"
+    end
     resources :items, only: %i[create index show destroy]
   end
   # Defines the root path route ("/")
