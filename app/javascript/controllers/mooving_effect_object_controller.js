97ce8e5d44c4bc46ea5d2a8b86feb2a24bdfe737
Alexis Velluet <alexis.velluet@outlook.com>
2023-03-13 21:20:57 +0100

    placed a floor in the floor scss and a transform in the  move object stimulus controller

--- app/javascript/controllers/mooving_effect_object_controller.js
+++ app/javascript/controllers/mooving_effect_object_controller.js
@@ -2,12 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="mooving-effect-object"
 export default class extends Controller {
-  static targets = ["object"]
+  static targets = ["object", "floor"]
 
   moovingEffect(event) {
 
     const mouseX = event.clientX / window.innerWidth * 4;
     const mouseY = event.clientY / window.innerHeight * 4;
     this.objectTargets.forEach(object => {object.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`});
+    this.floorTargets.forEach(object => {object.style.transform = `perspective(14px) translate3d(-${mouseX}%, -${mouseY}%, 0) rotateX(3deg)`});
   }
 }
+
+;
