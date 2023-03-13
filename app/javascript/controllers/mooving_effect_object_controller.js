a251c991f85ff1075506a092fce54a015bdf777f
Alexis Velluet <alexis.velluet@outlook.com>
2023-03-13 23:27:41 +0100

    new and index style done, css on wait page for list and form_legend for new,both on zindex2. new class on background glitch for linear gradient

--- app/javascript/controllers/mooving_effect_object_controller.js
+++ app/javascript/controllers/mooving_effect_object_controller.js
@@ -6,11 +6,12 @@ export default class extends Controller {
 
   moovingEffect(event) {
 
-    const mouseX = event.clientX / window.innerWidth * 4;
-    const mouseY = event.clientY / window.innerHeight * 4;
-    this.objectTargets.forEach(object => {object.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`});
+    const mouseX = event.clientX / window.innerWidth * 2;
+    const mouseY = event.clientY / window.innerHeight * 2;
+    const mouseXup = event.clientX / window.innerWidth * 4;
+    const mouseYup = event.clientY / window.innerHeight * 4;
+    this.objectTargets.forEach(object => {object.style.transform = `translate3d(-${mouseXup}%, -${mouseYup}%, 0)`});
     this.floorTargets.forEach(object => {object.style.transform = `perspective(14px) translate3d(-${mouseX}%, -${mouseY}%, 0) rotateX(3deg)`});
   }
 }
-
 ;
