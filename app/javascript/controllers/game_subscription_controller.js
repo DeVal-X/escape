6af9d9dea9a538da69dca85d058616897bbe7013
Alexis Velluet <alexis.velluet@outlook.com>
2023-03-14 17:58:20 +0100

    mid commit toglle class gamesubscription

--- app/javascript/controllers/game_subscription_controller.js
+++ app/javascript/controllers/game_subscription_controller.js
@@ -15,9 +15,52 @@ export default class extends Controller {
     "gameEnded",
     "gameLevel1",
     "gameLevel2",
-    "gameScore"
+    "gameScore",
+    'doorUser1',
+    'carpetUser1',
+    'chairUser1',
+    'floorUser1',
+    'frame101User1',
+    'frame102User1',
+    'frame103User1',
+    'frame104User1',
+    'frame105User1',
+    'frame1And2User1',
+    'frame3User1',
   ]
 
+  displayRoom1(addClass = true) {
+    this.Target.classList.toggle('fade-in-chandelier-n-r', addClass)
+    this.Target.classList.toggle('fade-in-door-n-r', addClass)
+    this.Target.classList.toggle('fade-in-chandelier-n', addClass)
+    this.Target.classList.toggle('chair', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+    this.Target.classList.toggle('chandelier', addClass)
+  }
+
+  hideRoom1() {
+    this.displayRoom1(false)
+    this.frame1Target.classList.add('fade-in-chandelier-n-r')
+    this.targets.classlist.add('fade-in-door-n-r')
+    this.targets.classlist.add('fade-in-chandelier-n-r')
+    this.targets.classlist.add('fade-in-carpet-n-r')
+    this.targets.classlist.add('fade-in-chair-e-r')
+    this.targets.classlist.add('fade-in-floor-s-r')
+    this.targets.classlist.add('fade-in-frame101-w-r')
+    this.targets.classlist.add('fade-in-frame102-e-r')
+    this.targets.classlist.add('fade-in-frame103-w-r')
+    this.targets.classlist.add('fade-in-frame104-e-r')
+    this.targets.classlist.add('fade-in-frame105-e-r')
+    this.targets.classlist.add('fade-in-frame1-2-w-r')
+    this.targets.classlist.add('fade-in-frame3-e-r')
+  }
 
   connect() {
     this.channel = createConsumer().subscriptions.create(
@@ -37,12 +80,11 @@ export default class extends Controller {
     }
 
     if (data.last_event === "start-game") {
-      const customGameEvent = new CustomEvent('game:start')
-      document.dispatchEvent(customGameEvent)
+      // const customGameEvent = new CustomEvent('game:start')
+      // document.dispatchEvent(customGameEvent)
       if (this.hasLobbyFullTarget ) { this.lobbyFullTarget.classList.add("d-none") }
       if (this.hasGameLevel1Target ) {
-        this.gameLevel1Target.classList.remove("d-none", "hidden-object")
-        this.gameLevel1Target.classList.add("displayed-object")
+        this.gameLevel1Target.classList.remove("d-none")
       }
     }
 
@@ -53,7 +95,7 @@ export default class extends Controller {
     }
 
     if (data.last_event === "success-open-door-one") {
-      if (this.hasGameLevel1Target )this.gameLevel1Target.classList.add("d-none")
+      if (this.hasGameLevel1Target) this.hideRoom1()
       if (this.hasGameLevel2Target) this.gameLevel2Target.classList.remove("d-none")
     }
 
--- app/javascript/controllers/mooving_effect_object_controller.js
+++ app/javascript/controllers/mooving_effect_object_controller.js
@@ -5,10 +5,10 @@ export default class extends Controller {
   static targets = ["object", "floor", "door", "carpet"]
 
   connect() {
-    console.log('coucou je suis le moving controller')
-    document.addEventListener('game:start', () => {
-      console.log('coucou je suis le moving controller et je suis declenché au start')
-    })
+    // console.log('coucou je suis le moving controller')
+    // document.addEventListener('game:start', () => {
+    //   console.log('coucou je suis le moving controller et je suis declenché au start')
+    // })
     const mouseX = 0
     const mouseY = 0
     const mouseXdown = 0
@@ -22,15 +22,15 @@ export default class extends Controller {
   }
 
   moovingEffect(event) {
-    const mouseX = event.clientX / window.innerWidth * 0.3;
-    const mouseY = event.clientY / window.innerHeight * 0.3;
+    const mouseX = event.clientX / window.innerWidth * 0.8;
+    const mouseY = event.clientY / window.innerHeight * 0.8;
     const mouseXdown = event.clientX / window.innerWidth * 2;
     const mouseYdown = event.clientY / window.innerHeight * 2;
     const mouseXup = event.clientX / window.innerWidth * 4;
     const mouseYup = event.clientY / window.innerHeight * 4;
     this.objectTargets.forEach(object => {object.style.transform = `translate3d(-${mouseXup}%, -${mouseYup}%, 0)`});
     this.doorTargets.forEach(object => {object.style.transform = `translate3d(-${mouseXdown}%, -${mouseYdown}%, 0)`});
-    this.carpetTargets.forEach(object => {object.style.transform = `translate3d(-${mouseXdown}%, -${mouseYdown}%, 0)`});
+    this.carpetTargets.forEach(object => {object.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`});
     this.floorTargets.forEach(object => {object.style.transform = `perspective(14px) translate3d(-${mouseX}%, -${mouseY}%, 0) rotateX(3deg)`});
   }
 }
