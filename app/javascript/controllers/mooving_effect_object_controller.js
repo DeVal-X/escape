127281b3aa48338406a6e2b3dbf3bc7b56746da2
Léa Besse <122352275+Viewfinderr@users.noreply.github.com>
2023-03-14 15:00:03 +0100

    l'effet noisy est ok

--- app/javascript/controllers/mooving_effect_object_controller.js
+++ app/javascript/controllers/mooving_effect_object_controller.js
@@ -5,7 +5,6 @@ export default class extends Controller {
   static targets = ["object"]
 
   moovingEffect(event) {
-
     const mouseX = event.clientX / window.innerWidth * 4;
     const mouseY = event.clientY / window.innerHeight * 4;
     this.objectTarget.style.transform = `translate3d(-${mouseX}%, -${mouseY}%, 0)`;
