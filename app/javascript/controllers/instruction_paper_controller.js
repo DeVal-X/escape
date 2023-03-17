79ce490c9a896d3f4143d5ebcf44c190c850fe17
david rabuteau <david.rabuteau@hotmail.fr>
2023-03-17 11:57:42 +0100

    petite modif de fou paf

--- app/javascript/controllers/instruction_paper_controller.js
+++ app/javascript/controllers/instruction_paper_controller.js
@@ -3,6 +3,6 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="instruction-paper"
 export default class extends Controller {
   open() {
-      window.alert( "Finds the keys, the first with several legs, the second with warm and the third drowns ... Only one key opens the door, the others active traps.." )
+      window.alert( "bha écoute je suis arrivé dans un salon, il y a pas mal d'informations. J'ai un bout de papier devant moi avec une petite note, je te la lis tout de suite : Elle se cache dans le savoir et vous donnera espoir")
   }
 }
